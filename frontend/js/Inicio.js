28c3476ac2ebb7bc73f9fcf589e1a11a701ec7bd
Htrox <103452732+Htrox@users.noreply.github.com>
2025-11-14 12:25:03 +0100

    Update Inicio.js

--- frontend/js/Inicio.js
+++ frontend/js/Inicio.js
@@ -1,1 +1,2 @@
-document.getElementById("descuentos").innerHTML = "<a href=''><img src='../img/logo.png' alt='descuento' style='width: 100%; height: 100%'></a>"
+document.getElementById("descuentos").innerHTML = "<a href=''>"
+document.getElementById("descuentos").innerHTML += "<img src='../img/logo.png' alt='descuento' id='img_descuentos'></a>"
