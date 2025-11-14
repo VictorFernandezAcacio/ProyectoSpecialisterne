cc06e5058e0ab81b99da71506d5bf1d3141ae41d
Htrox <103452732+Htrox@users.noreply.github.com>
2025-11-14 09:39:30 +0100

    Add files via upload

---
+++ frontend/js/Viaje.js
@@ -0,0 +1,6 @@
+function Mostrar_Ventana_Reseña() {
+    document.getElementById('alerta_reseña').style.display = 'block';
+}
+function Quitar_Ventana_Reseña() {
+    document.getElementById('alerta_reseña').style.display = 'none';
+}
