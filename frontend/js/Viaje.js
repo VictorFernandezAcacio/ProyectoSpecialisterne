a1616d590dab07c94f72b8604557aa8f45b355f5
Htrox <103452732+Htrox@users.noreply.github.com>
2025-11-14 10:46:41 +0100

    Update Viaje.js

--- frontend/js/Viaje.js
+++ frontend/js/Viaje.js
@@ -1,6 +1,23 @@
+Reseña = {
+    Fecha: "",
+    Estrellas: 0,
+    Comentario: "",
+}
+
 function Mostrar_Ventana_Reseña() {
     document.getElementById('alerta_reseña').style.display = 'block';
 }
 function Quitar_Ventana_Reseña() {
     document.getElementById('alerta_reseña').style.display = 'none';
+}
+
+
+
+function RESEÑAR() {
+    Reseña.Fecha = document.getElementById("fehca_reseña").value
+    Reseña.Estrellas = document.getElementById("estrellas_reseña").value
+    Reseña.Comentario = document.getElementById("comentario_reseña").value
+
+
+    console.log(Reseña)
 }
