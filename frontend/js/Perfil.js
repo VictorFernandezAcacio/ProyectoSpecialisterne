9f05f3a98342cbca1ee23e37e9251498fabaf376
VictorFernandezAcacio <vfernandezacacio790@gmail.com>
2025-11-17 09:30:05 +0100

    Delete frontend/js/Perfil.js

--- frontend/js/Perfil.js
+++
@@ -1,6 +0,0 @@
-function Mostrar_Ventana_Borrar() {
-    document.getElementById('alerta_borrar').style.display = 'block';
-}
-function Quitar_Ventana_Borrar() {
-    document.getElementById('alerta_borrar').style.display = 'none';
-}
