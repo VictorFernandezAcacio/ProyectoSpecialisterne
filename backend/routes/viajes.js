c0834d98b7801c0af181cd706ffa38956e45aa8c
VictorFernandezAcacio <vfernandezacacio790@gmail.com>
2025-12-09 13:23:21 +0100

    Add files via upload

--- backend/routes/viajes.js
+++ backend/routes/viajes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const viajesController = require('../controllers/viajesController');
+const verificarAdmin = require('../../middleware/verificarAdmin');
 
 // CRUD de viajes
-router.post('/', viajesController.crearViaje);
+router.post('/', verificarAdmin, viajesController.crearViaje);
 router.get('/', viajesController.obtenerViajes);
 router.get('/:id', viajesController.obtenerViaje);
-router.put('/:id', viajesController.actualizarViaje);
-router.delete('/:id', viajesController.eliminarViaje);
+router.put('/:id', verificarAdmin, viajesController.actualizarViaje);
+router.delete('/:id', verificarAdmin, viajesController.eliminarViaje);
 
-// Extras
+// Extras (públicos)
 router.get('/:id/resenas', viajesController.obtenerResenasViaje);
 router.get('/:id/alojamientos', viajesController.obtenerAlojamientosViaje);
 router.get('/:id/transportes', viajesController.obtenerTransportesViaje);
