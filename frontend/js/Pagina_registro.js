3b111aacf8e7a596a3f03926411b545eaf9658ad
Artemiy-git <artemyzhukov@gmail.com>
2025-12-16 10:30:25 +0100

    Fix registration logic and update date validation

--- frontend/js/Pagina_registro.js
+++ frontend/js/Pagina_registro.js
@@ -28,14 +28,14 @@ async function registrarse() {
   const contrasenya = document.getElementById("contrasenya").value.trim();
   const confirmar = document.getElementById("confirmar_contrasenya").value.trim();
   const fecha_nacimiento = document.getElementById("fecha_de_nacimiento").value;
-  const fecha_nacimiento_date = new Date ("fecha_nacimiento")
+  const fecha_nacimiento_date = new Date (fecha_nacimiento)
 
   if (!usuario || !correo || !contrasenya || !confirmar || !fecha_nacimiento) return mostrarError("Por favor, complete todos los campos.");
   if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(correo)) return mostrarError("El correo no parece válido.");
   if (contrasenya.length < 8) return mostrarError("La contraseña es demasiado corta.");
   if (esMenorDe18(fecha_nacimiento)) return mostrarError
   ("Los menores de 18 años no pueden registrarse. ¡Le esperamos más tarde!");
-  if (fecha_nacimiento_date < new Date("1909-04-21")) return mostrarError ("Por favor, introduzca la fecha real.");
+  if (fecha_nacimiento_date < new Date("1909-04-24")) return mostrarError ("Por favor, introduzca la fecha real.");
   if (contrasenya !== confirmar) return mostrarError("Las contraseñas no coinciden.");
 
   try {
@@ -70,4 +70,5 @@ function volverLogin() {
 // Exponer funciones al scope global
 window.registrarse = registrarse;
 window.volverLogin = volverLogin;
+
 
